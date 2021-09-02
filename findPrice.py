661888f971b1916bd1e209792f3079e79534f5a2
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-09-02 22:40:10 +0700

    Can update data directly to Main.xlsx

--- findPrice.py
+++ findPrice.py
@@ -5,7 +5,7 @@ from openpyxl.utils.cell import get_column_letter
 
 workbook = openpyxl.load_workbook(filename="Main.xlsx")
 sheet = workbook.active
-number_rows = 4
+number_rows = 3
 number_columns = sheet.max_column
 
 
@@ -49,7 +49,6 @@ for i in range(1,number_rows):
             tickerDateP4 = str(tickerSheet["A"+str(k+5)].value)
             tickerDateP5 = str(tickerSheet["A"+str(k+6)].value)
 
-
             tickerCPEndDateM5 = str(tickerSheet["E"+str(k-4)].value)
             tickerCPEndDateM4 = str(tickerSheet["E"+str(k-3)].value)
             tickerCPEndDateM3 = str(tickerSheet["E"+str(k-2)].value)
@@ -62,6 +61,18 @@ for i in range(1,number_rows):
             tickerCPEndDateP4 = str(tickerSheet["E"+str(k+5)].value)
             tickerCPEndDateP5 = str(tickerSheet["E"+str(k+6)].value)
             
+            sheet["F"+str(i+1)] = tickerCPEndDateM5
+            sheet["G"+str(i+1)] = tickerCPEndDateM4
+            sheet["H"+str(i+1)] = tickerCPEndDateM3
+            sheet["I"+str(i+1)] = tickerCPEndDateM2
+            sheet["J"+str(i+1)] = tickerCPEndDateM1
+            sheet["K"+str(i+1)] = tickerCPEndDate
+            sheet["L"+str(i+1)] = tickerCPEndDateP1
+            sheet["M"+str(i+1)] = tickerCPEndDateP2
+            sheet["N"+str(i+1)] = tickerCPEndDateP3
+            sheet["O"+str(i+1)] = tickerCPEndDateP4
+            sheet["P"+str(i+1)] = tickerCPEndDateP5
+
             print(tickerName + " " + tickerDateM5 + " " + tickerCPEndDateM5)
             print(tickerName + " " + tickerDateM4 + " " + tickerCPEndDateM4)
             print(tickerName + " " + tickerDateM3 + " " + tickerCPEndDateM3)
@@ -73,9 +84,8 @@ for i in range(1,number_rows):
             print(tickerName + " " + tickerDateP3 + " " + tickerCPEndDateP3)
             print(tickerName + " " + tickerDateP4 + " " + tickerCPEndDateP4)
 
-
             break
         k += 1
     print("-------------------------------")
 
-# workbook.save("Main.xlsx")
+workbook.save("Main.xlsx")
